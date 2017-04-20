712f45fd244ec4a4192ff999756ce4cf70fe072c
kyle webb <kylewebb4@weber.edu>
2017-04-20 08:41:49 -0600

    first commit

---
+++ kylewebb_exam2_task1.c
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  kylewebb_exam2_task1.c
+ *       	Usage:  ./kylewebb_exam2_task1.c
+ *    Description:  Task 1 on exam sheet
+ *
+ *        Version:  1.0
+ *        Created:  04/20/2017 08:40:50 AM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Kyle Webb (), kylewebbear@weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>		/* Standard Library */
+
+/* Function Prototypes */
+
+/* Main Program */
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
